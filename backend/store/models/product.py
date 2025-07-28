bf7876431c74d697338df0d25b9e6f9bc9396cee
mcieslik <01187214@pw.edu.pl>
2025-07-28 11:56:31 +0200

    Added status enum

--- backend/store/models/product.py
+++ backend/store/models/product.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.db.models.enums import TextChoices
+
 
 class Product(models.Model):
+
+    class ProductStatus(TextChoices):
+        AVAILABLE = "available"
+        UNAVAILABLE = "unavailable"
+        ARCHIVED = "archived"
+
     product_id = models.AutoField(primary_key=True)
     owner_id = models.IntegerField(null=False)
     name = models.CharField(max_length=64, null=False)
@@ -13,6 +21,7 @@ class Product(models.Model):
     width = models.DecimalField(max_digits=5, decimal_places=2, null=False)
     height = models.DecimalField(max_digits=5, decimal_places=2, null=False)
     guarantee_period = models.IntegerField(null=False)
+    status = models.CharField(max_length=20, choices=ProductStatus.choices, default=ProductStatus.AVAILABLE, null=False)
 
     def __str__(self):
-        return "Name: " + str(self.name)
+        return "Name: " + str(self)
