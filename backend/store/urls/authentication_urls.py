bcf6913ce51880454424902324194685bc1e4246
mcieslik <01187214@pw.edu.pl>
2025-07-30 23:48:41 +0200

    Changed classes names

--- backend/store/urls/authentication_urls.py
+++ backend/store/urls/authentication_urls.py
@@ -4,6 +4,6 @@ from backend.store.views.authentication_view import *
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
-    path("verify/", VerifyView.as_view(), name="verify"),
-    path("refresh/", RefreshView.as_view(), name="refresh")
+    path("verify/", VerifyTokenView.as_view(), name="verify"),
+    path("refresh/", RefreshTokenView.as_view(), name="refresh")
 ]
