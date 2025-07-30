514e460eb50927e99ef0a239f21799deb155b640
mcieslik <01187214@pw.edu.pl>
2025-07-30 23:46:05 +0200

    Aded new exceptions

--- backend/store/exceptions.py
+++ backend/store/exceptions.py
@@ -1,9 +1,19 @@
 from rest_framework.exceptions import ValidationError
 
-
 class EmailAlreadyTakenError(ValidationError):
     pass
 
-
 class UsernameAlreadyTakenError(ValidationError):
+    pass
+
+class MissingEmailError(ValidationError):
+    pass
+
+class MissingPasswordError(ValidationError):
+    pass
+
+class UserNotFoundError(ValidationError):
+    pass
+
+class InvalidPasswordError(ValidationError):
     pass
