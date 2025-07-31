0e9eba1148e246535d6d655ed57c0abdf55a7ba7
mcieslik <01187214@pw.edu.pl>
2025-07-31 16:04:15 +0200

    Added email sending and secrets

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 
+from .secrets import EMAIL_HOST_USER_SECRET, EMAIL_HOST_PASSWORD_SECRET
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -143,3 +145,11 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = EMAIL_HOST_USER_SECRET
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD_SECRET
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
