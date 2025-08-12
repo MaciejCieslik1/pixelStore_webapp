8ef103c34cbe889adfb0f4fe05b858b8229674bf
mcieslik <01187214@pw.edu.pl>
2025-08-12 14:54:43 +0200

    Created new packages structure for serializers

---
+++ backend/store/serializers/authentication_serializer.py
@@ -0,0 +1,20 @@
+class UserRegisterSerializer:
+    def __init__(self, data: dict):
+        self._data = data
+        self._validated_data = {}
+        self._errors = {}
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def validated_data(self):
+        return self._validated_data
+
+    @property
+    def errors(self):
+        return self.errors
+
+    def is_valid(self) -> bool:
+        return False
