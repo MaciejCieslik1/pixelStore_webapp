67005e3ac2a35c0d5836896b968ba9e27513bbc9
mcieslik <01187214@pw.edu.pl>
2025-08-13 18:08:35 +0200

    Added serializer tests

--- backend/store/tests/tests_serializer/test_authentication_serializer.py
+++ backend/store/tests/tests_serializer/test_authentication_serializer.py
@@ -1,5 +1,16 @@
 import unittest
 
+from store.serializers.authentication_serializer import RegisterSerializer
+
 
 class TestRegisterSerializer(unittest.TestCase):
-    pass
+    def test_register_correct_data(self):
+        data = {"email": "test@example.com", "username": "tester", "password": "Abcdefg1#abc", "is_verified": False,
+                "bio": "I'm new here!", "money": 0.00, "is_superuser": False, "last_login": None,
+                "address": "fweffwe", "postal_code": "00001", "city": "Warsaw", "country": "Poland"}
+
+        serializer = RegisterSerializer(data=data)
+
+        self.assertEqual(serializer.is_valid(), True)
+        self.assertEqual(serializer.validated_data, data)
+        self.assertEqual(serializer.errors, {})
