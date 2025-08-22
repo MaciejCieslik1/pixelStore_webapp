6448e1e325214e7ae4ee86e7da346ec48e70032e
mcieslik <01187214@pw.edu.pl>
2025-08-22 10:10:18 +0200

    Added address service template

--- backend/store/service/address_service.py
+++ backend/store/service/address_service.py
@@ -1,8 +1,11 @@
+from store.models import User
+
+
 class FindAddressService:
-    def find(self):
+    def find(self, token: str, user: User):
         pass
 
 
 class UpdateAddressService:
-    def update(self):
+    def update(self, token: str, user: User, new_address_data: dict):
         pass
