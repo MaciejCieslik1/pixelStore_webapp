60e7b9c80315c06294295ccaeedb6b9a7a149153
mcieslik <01187214@pw.edu.pl>
2025-08-23 10:18:45 +0200

    Added category service template

---
+++ backend/store/service/category_service.py
@@ -0,0 +1,13 @@
+class FindCategoryByNameService:
+    def find_category_by_name(self, token: str, name: str) -> dict:
+        pass
+
+
+class FindAllCategoriesService:
+    def find_all_categories(self, token: str) -> dict:
+        pass
+
+
+class CreateCategoryService:
+    def create_category(self, token: str, new_category_data: dict) -> dict:
+        pass
