b60fdf2ae2cfb30c69a8fda2f537d8b5c259ce73
mcieslik <01187214@pw.edu.pl>
2025-08-23 10:18:55 +0200

    Added category serializer template

---
+++ backend/store/serializers/category_serializer.py
@@ -0,0 +1,64 @@
+class FindCategoryByNameSerializer:
+    def __init__(self, data: dict):
+        self._data = data
+        self._validated_data = {}
+        self._errors = {}
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def validated_data(self):
+        return self._validated_data
+
+    @property
+    def errors(self):
+        return self._errors
+
+    def is_valid(self) -> bool:
+        return True
+
+
+class FindAllCategoriesSerializer:
+    def __init__(self, data: dict):
+        self._data = data
+        self._validated_data = {}
+        self._errors = {}
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def validated_data(self):
+        return self._validated_data
+
+    @property
+    def errors(self):
+        return self._errors
+
+    def is_valid(self) -> bool:
+        return True
+
+
+class CreateCategorySerializer:
+    def __init__(self, data: dict):
+        self._data = data
+        self._validated_data = {}
+        self._errors = {}
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def validated_data(self):
+        return self._validated_data
+
+    @property
+    def errors(self):
+        return self._errors
+
+    def is_valid(self) -> bool:
+        return True
