e85756faf4f17147d59542c4a44ecbbeaed26592
mcieslik <01187214@pw.edu.pl>
2025-08-30 15:51:10 +0200

    Added category serializer

--- backend/store/serializers/category_serializer.py
+++ backend/store/serializers/category_serializer.py
@@ -1,3 +1,6 @@
+from store.helper_classes.authentication_helper import DataValidator
+
+
 class FindCategoryByNameSerializer:
     def __init__(self, name: str):
         self._name = name
@@ -16,7 +19,29 @@ class FindCategoryByNameSerializer:
     def error(self):
         return self._error
 
+    @validated_name.setter
+    def validated_name(self, new_validated_name):
+        self._validated_name = new_validated_name
+
+    @error.setter
+    def error(self, new_error):
+        self._error = new_error
+
+
     def is_valid(self) -> bool:
+        min_length = 0
+        max_length = 32
+        name = self.name
+
+        if name is None or not name.strip():
+            self.error = "Category name is not provided."
+            return False
+        name = name.strip()
+        if not DataValidator.validate_length(name, min_length, max_length):
+            self.error = "Category name cannot be longer than 32 characters."
+            return False
+
+        self.validated_name = name
         return True
 
 
@@ -39,4 +64,36 @@ class CreateCategorySerializer:
         return self._errors
 
     def is_valid(self) -> bool:
+        return self._validate_name() and self._validate_description()
+
+    def _validate_name(self):
+        min_length = 0
+        max_length = 32
+        name = self.data.get("name")
+
+        if name is None or not name.strip():
+            self.errors["name"] = "Category name is not provided."
+            return False
+        name = name.strip()
+        if not DataValidator.validate_length(name, min_length, max_length):
+            self.errors["name"] = "Category name cannot be longer than 32 characters."
+            return False
+
+        self.validated_data["name"] = name
+        return True
+
+    def _validate_description(self):
+        min_length = 0
+        max_length = 1024
+        description = self.data.get("description")
+
+        if description is None or not description.strip():
+            self.errors["description"] = "Description is not provided."
+            return False
+        description = description.strip()
+        if not DataValidator.validate_length(description, min_length, max_length):
+            self.errors["description"] = "Description cannot be longer than 1024 characters."
+            return False
+
+        self.validated_data["description"] = description
         return True
