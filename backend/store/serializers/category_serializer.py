9bd2750cd66b2a0b6d1d3a093e57f27a43ae444b
mcieslik <01187214@pw.edu.pl>
2025-08-30 11:42:35 +0200

    Renamed fields in serializer

--- backend/store/serializers/category_serializer.py
+++ backend/store/serializers/category_serializer.py
@@ -1,42 +1,20 @@
 class FindCategoryByNameSerializer:
-    def __init__(self, data: dict):
-        self._data = data
-        self._validated_data = {}
-        self._errors = {}
+    def __init__(self, name: str):
+        self._name = name
+        self._validated_name = None
+        self._error = None
 
     @property
-    def data(self):
-        return self._data
+    def name(self):
+        return self._name
 
     @property
-    def validated_data(self):
-        return self._validated_data
+    def validated_name(self):
+        return self._validated_name
 
     @property
-    def errors(self):
-        return self._errors
-
-    def is_valid(self) -> bool:
-        return True
-
-
-class FindAllCategoriesSerializer:
-    def __init__(self, data: dict):
-        self._data = data
-        self._validated_data = {}
-        self._errors = {}
-
-    @property
-    def data(self):
-        return self._data
-
-    @property
-    def validated_data(self):
-        return self._validated_data
-
-    @property
-    def errors(self):
-        return self._errors
+    def error(self):
+        return self._error
 
     def is_valid(self) -> bool:
         return True
