03311fbd55bcfaee318d47554b4e2f1909a47052
mcieslik <01187214@pw.edu.pl>
2025-08-30 11:42:18 +0200

    Renamed endpoints

--- backend/store/urls/category_urls.py
+++ backend/store/urls/category_urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-
-from store.service.category_service import *
 from store.views.category_view import *
 
 find_category_by_name_service = FindCategoryByNameService()
@@ -8,10 +6,10 @@ find_all_categories_service = FindAllCategoriesService()
 create_category_service = CreateCategoryService()
 
 urlpatterns = [
-    path("find_category_by_name/", FindCategoryByNameView.as_view(find_category_by_name_service=find_category_by_name_service),
-         name="category_find_category_by_name/<str:name>/"),
-    path("find_all_categories/", FindAllCategoriesView.as_view(find_all_categories_service=find_all_categories_service),
-         name="category_find_all_categories"),
-    path("create_category/", CreateCategoryView.as_view(create_category_service=create_category_service),
-         name="category_create_category"),
+    path("find_by_name/<str:name>/", FindCategoryByNameView.as_view(find_category_by_name_service=find_category_by_name_service),
+         name="category_find_by_name/<str:name>/"),
+    path("find_all/", FindAllCategoriesView.as_view(find_all_categories_service=find_all_categories_service),
+         name="category_find_all"),
+    path("create/", CreateCategoryView.as_view(create_category_service=create_category_service),
+         name="category_create"),
 ]
