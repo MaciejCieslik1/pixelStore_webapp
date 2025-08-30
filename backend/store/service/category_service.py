668aa39c871c6035898b67ef7ee576fe2dc75dde
mcieslik <01187214@pw.edu.pl>
2025-08-30 11:42:47 +0200

    Changed names of functions

--- backend/store/service/category_service.py
+++ backend/store/service/category_service.py
@@ -1,13 +1,13 @@
 class FindCategoryByNameService:
-    def find_category_by_name(self, token: str, name: str) -> dict:
+    def find_by_name(self, token: str, name: str) -> dict:
         pass
 
 
 class FindAllCategoriesService:
-    def find_all_categories(self, token: str) -> dict:
+    def find_all(self, token: str) -> dict:
         pass
 
 
 class CreateCategoryService:
-    def create_category(self, token: str, new_category_data: dict) -> dict:
+    def create(self, token: str, new_category_data: dict) -> dict:
         pass
