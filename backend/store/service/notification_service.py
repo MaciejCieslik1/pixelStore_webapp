436276382d56ec110d9b2663f1509756326e983e
mcieslik <01187214@pw.edu.pl>
2025-09-02 18:23:41 +0200

    Added query data

--- backend/store/service/notification_service.py
+++ backend/store/service/notification_service.py
@@ -5,8 +5,8 @@ from store.models import User
 
 
 class FindAllNotificationsService:
-    def find_all(self, token: str, date_from: Optional[date] = None, date_to: Optional[date] = None, order: str = "desc",
-                 page: int = 1, page_size: int = 10):
+    def find_all(self, token: str,  data: dict, date_from: Optional[date] = None, date_to: Optional[date] = None,
+                order: str = "desc", page: int = 1, page_size: int = 10):
         pass
 
 
