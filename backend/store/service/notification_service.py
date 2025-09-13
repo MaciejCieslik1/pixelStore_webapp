dbc034b4227eac94fa439a2aec6a70d110ad0316
mcieslik <01187214@pw.edu.pl>
2025-09-13 11:52:42 +0200

    added notification service

--- backend/store/service/notification_service.py
+++ backend/store/service/notification_service.py
@@ -1,20 +1,71 @@
-from datetime import date
-from typing import Optional
+from django.core.paginator import Paginator
+from django.utils import timezone
 
-from store.models import User
+from store.exceptions import NotificationIdDoesNotBelongToUserError, SelfUsernameError, InvalidUsernameError, \
+    InvalidNotificationIdError
+from store.helper_classes.authentication_helper import TokenUtils
+from store.models import User, Notification
 
 
 class FindAllNotificationsService:
-    def find_all(self, token: str,  data: dict, date_from: Optional[date] = None, date_to: Optional[date] = None,
-                order: str = "desc", page: int = 1, page_size: int = 10):
-        pass
+    def find_all(self, token: str, user: User, validated_data: dict):
+        TokenUtils.verify_access_token(token, user)
+        date_from = validated_data.get("date_from")
+        date_to = validated_data.get("date_to")
+        order = validated_data.get("order")
+        page = validated_data.get("page")
+        page_size = validated_data.get("page_size")
+
+        notifications = Notification.objects.filter(receiver=user)
+
+        if date_from:
+            notifications = notifications.filter(sent_date_time__gte=date_from)
+        if date_to:
+            notifications = notifications.filter(sent_date_time__lte=date_to)
+
+        if order == "desc":
+            notifications = notifications.order_by("-sent_date_time", "notification_id")
+        else:
+            notifications = notifications.order_by("sent_date_time", "notification_id")
+
+        response = [{"username": notification.sender.username, "sent_date_time": notification.sent_date_time,
+                "text": notification.text} for notification in notifications]
+
+        paginator = Paginator(response, page_size)
+        page_obj = paginator.get_page(page)
+
+        return page_obj.object_list
 
 
 class CreateNotificationService:
-    def create(self, token: str, user: User, data: dict):
-        pass
+    def create(self, token: str, user: User, validated_data: dict):
+        TokenUtils.verify_access_token(token, user)
+        username = validated_data.get("username")
+
+        if user.username == username:
+            raise SelfUsernameError("Self username provided.")
+
+        user_with_username = User.objects.filter(username=username)
+        if not user_with_username:
+            raise InvalidUsernameError("Invalid username of a notification receiver.")
+
+        receiver = User.objects.get(username=username)
+        notification = Notification.objects.create(sender=user, receiver=receiver, sent_date_time=timezone.now(),
+            text=validated_data["text"])
+        notification.save()
+        return "Notification created successfully."
 
 
 class DeleteNotificationService:
-    def delete(self, token: str, user: User, data: dict):
-        pass
+    def delete(self, token: str, user: User, validated_data: dict):
+        TokenUtils.verify_access_token(token, user)
+        notification = Notification.objects.filter(notification_id=validated_data.get("notification_id")).first()
+
+        if not notification:
+            raise InvalidNotificationIdError("Invalid notification id.")
+
+        if notification.sender.user_id != user.user_id and notification.receiver.user_id != user.user_id:
+            raise NotificationIdDoesNotBelongToUserError("Notification with given id does not belong to the user.")
+
+        notification.delete()
+        return "Notification deleted successfully."
