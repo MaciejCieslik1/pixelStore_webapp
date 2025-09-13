2bc0fffc1876d6f50ad99c63abd53a197d31b02b
mcieslik <01187214@pw.edu.pl>
2025-09-13 15:40:07 +0200

    Added template for order_product service

---
+++ backend/store/service/order_product_service.py
@@ -0,0 +1,17 @@
+class FindByIdOrderProductService:
+    def find_by_id(self):
+        pass
+
+class CreateOrderProductService:
+    def create(self):
+        pass
+
+
+class UpdateOrderProductService:
+    def update(self):
+        pass
+
+
+class DeleteOrderProductService:
+    def delete(self):
+        pass
