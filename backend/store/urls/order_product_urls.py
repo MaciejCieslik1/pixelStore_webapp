e54e087b4d4445a71d31330bc2454d529dbed66f
mcieslik <01187214@pw.edu.pl>
2025-09-13 15:39:44 +0200

    Added new endpoints for order_product

---
+++ backend/store/urls/order_product_urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+
+from store.service.order_product_service import FindByIdOrderProductService, CreateOrderProductService, \
+    UpdateOrderProductService, DeleteOrderProductService
+from store.views.order_product_view import FindByIdOrderProductView, CreateOrderProductView, UpdateOrderProductView, \
+    DeleteOrderProductView
+
+find_by_id_order_product_service = FindByIdOrderProductService()
+create_service = CreateOrderProductService()
+update_service = UpdateOrderProductService()
+delete_service = DeleteOrderProductService()
+
+
+urlpatterns = [
+    path("find_by_id/<int:order_product_id>/", FindByIdOrderProductView.as_view(
+        find_by_id_order_product_service=find_by_id_order_product_service), name="find_by_id_order_product/<int:order_product_id>/"),
+    path("create/", CreateOrderProductView.as_view(create_order_product_service=create_service),
+         name="create_order_product"),
+    path("update/", UpdateOrderProductView.as_view(update_order_product_service=update_service),
+         name="update_order_product"),
+    path("delete/", DeleteOrderProductView.as_view(delete_order_product_service=delete_service),
+         name="delete_order_product"),
+]
