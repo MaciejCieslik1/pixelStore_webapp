dc6a3e1dc61602a65aa5f72a583ef3eb7caf1a01
mcieslik <01187214@pw.edu.pl>
2025-09-14 16:15:31 +0200

    Added method parameters

--- backend/store/service/order_product_service.py
+++ backend/store/service/order_product_service.py
@@ -1,17 +1,20 @@
+from store.models import User
+
+
 class FindByIdOrderProductService:
-    def find_by_id(self):
+    def find_by_id(self, token: str, user: User, order_product_id: int) -> dict:
         pass
 
 class CreateOrderProductService:
-    def create(self):
+    def create(self, token: str, user: User, new_order_product_data: dict) -> str:
         pass
 
 
 class UpdateOrderProductService:
-    def update(self):
+    def update(self, token: str,user: User, updated_order_product_data: dict) -> str:
         pass
 
 
 class DeleteOrderProductService:
-    def delete(self):
+    def delete(self, token : str, user: User, order_product_id: int) -> str:
         pass
