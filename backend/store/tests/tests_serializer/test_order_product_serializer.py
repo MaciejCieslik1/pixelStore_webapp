77f0a4bdcc6a3b7c99cb419434436ea36264d49f
mcieslik <01187214@pw.edu.pl>
2025-09-15 11:30:24 +0200

    Fixed some tests issues

--- backend/store/tests/tests_serializer/test_order_product_serializer.py
+++ backend/store/tests/tests_serializer/test_order_product_serializer.py
@@ -1,13 +1,13 @@
 import unittest
 
-from store.serializers.order_product_serializer import FindByIdOrderProductSerializer, DeleteOrderProductSerializer, \
-    CreateOrderProductSerializer, UpdateOrderProductSerializer
+from store.serializers.order_product_serializer import (CreateOrderProductSerializer, UpdateOrderProductSerializer,
+    FindByIdAndDeleteOrderProductSerializer)
 
 
 class TestFindByIdOrderProductSerializer(unittest.TestCase):
     def test_find_by_id_success(self):
         order_product_id = 1
-        serializer = FindByIdOrderProductSerializer(order_product_id=order_product_id)
+        serializer = FindByIdAndDeleteOrderProductSerializer(order_product_id=order_product_id)
 
         result = serializer.is_valid()
 
@@ -16,7 +16,7 @@ class TestFindByIdOrderProductSerializer(unittest.TestCase):
 
     def test_find_by_id_empty_order_product_id(self):
         order_product_id = None
-        serializer = FindByIdOrderProductSerializer(order_product_id=order_product_id)
+        serializer = FindByIdAndDeleteOrderProductSerializer(order_product_id=order_product_id)
 
         result = serializer.is_valid()
 
@@ -25,7 +25,7 @@ class TestFindByIdOrderProductSerializer(unittest.TestCase):
 
     def test_find_by_id_string(self):
         order_product_id = "sfdfffs"
-        serializer = FindByIdOrderProductSerializer(order_product_id=order_product_id)
+        serializer = FindByIdAndDeleteOrderProductSerializer(order_product_id=order_product_id)
 
         result = serializer.is_valid()
 
@@ -34,7 +34,7 @@ class TestFindByIdOrderProductSerializer(unittest.TestCase):
 
     def test_find_by_id_not_positive_integer(self):
         order_product_id = 0
-        serializer = FindByIdOrderProductSerializer(order_product_id=order_product_id)
+        serializer = FindByIdAndDeleteOrderProductSerializer(order_product_id=order_product_id)
 
         result = serializer.is_valid()
 
@@ -68,23 +68,25 @@ class TestCreateOrderProductSerializer(unittest.TestCase):
 
     def test_create_shopping_price_max_integer(self):
         self.data["shopping_price"] = 999999
+        self.validated_data["shopping_price"] = 999999
         serializer = CreateOrderProductSerializer(data=self.data)
 
         result = serializer.is_valid()
 
-        self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive integer."})
+        self.assertTrue(result)
+        self.assertEqual(serializer.validated_data, self.validated_data)
+        self.assertEqual(serializer.errors, {})
 
     def test_create_shopping_price_max_decimal(self):
         self.data["shopping_price"] = 999999.99
+        self.validated_data["shopping_price"] = 999999.99
         serializer = CreateOrderProductSerializer(data=self.data)
 
         result = serializer.is_valid()
 
-        self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive integer."})
+        self.assertTrue(result)
+        self.assertEqual(serializer.validated_data, self.validated_data)
+        self.assertEqual(serializer.errors, {})
 
     def test_create_empty_product_id(self):
         self.data["product_id"] = None
@@ -124,7 +126,7 @@ class TestCreateOrderProductSerializer(unittest.TestCase):
 
         self.assertFalse(result)
         self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price username cannot be empty."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price cannot be empty."})
 
     def test_create_product_id_string(self):
         self.data["product_id"] = "sfdfffs"
@@ -157,7 +159,7 @@ class TestCreateOrderProductSerializer(unittest.TestCase):
         self.assertEqual(serializer.errors, {"transaction_id": "Transaction id must be positive integer."})
 
     def test_create_transaction_id_not_positive_integer(self):
-        self.data["product_id"] = 0
+        self.data["transaction_id"] = 0
         serializer = CreateOrderProductSerializer(data=self.data)
 
         result = serializer.is_valid()
@@ -176,7 +178,7 @@ class TestCreateOrderProductSerializer(unittest.TestCase):
         self.assertNotEqual(serializer.validated_data, self.validated_data)
         self.assertEqual(serializer.errors, {"seller_username": "Seller username must be string."})
 
-    def test_create_sopping_price_string(self):
+    def test_create_shopping_price_string(self):
         self.data["shopping_price"] = "sfdfffs"
         serializer = CreateOrderProductSerializer(data=self.data)
 
@@ -184,7 +186,7 @@ class TestCreateOrderProductSerializer(unittest.TestCase):
 
         self.assertFalse(result)
         self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive decimal number."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive integer or decimal."})
 
     def test_create_shopping_price_not_positive_integer(self):
         self.data["shopping_price"] = 0
@@ -194,7 +196,7 @@ class TestCreateOrderProductSerializer(unittest.TestCase):
 
         self.assertFalse(result)
         self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive decimal number."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive integer or decimal."})
 
     def test_create_shopping_price_too_big_integer(self):
         self.data["shopping_price"] = 1000000
@@ -204,7 +206,7 @@ class TestCreateOrderProductSerializer(unittest.TestCase):
 
         self.assertFalse(result)
         self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive decimal number."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be less than 1000000.00."})
 
     def test_create_shopping_price_too_big_decimal(self):
         self.data["shopping_price"] = 1000000.00
@@ -214,13 +216,12 @@ class TestCreateOrderProductSerializer(unittest.TestCase):
 
         self.assertFalse(result)
         self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive decimal number."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be less than 1000000.00."})
 
 
 class TestUpdateOrderProductSerializer(unittest.TestCase):
     def setUp(self):
         self.data = {"shopping_price": 1000}
-        self.validated_data = {"shopping_price": 1000}
 
     def test_update_success(self):
         serializer = UpdateOrderProductSerializer(data=self.data)
@@ -228,7 +229,6 @@ class TestUpdateOrderProductSerializer(unittest.TestCase):
         result = serializer.is_valid()
 
         self.assertTrue(result)
-        self.assertEqual(serializer.validated_data, self.validated_data)
         self.assertEqual(serializer.errors, {})
 
     def test_update_shopping_price_max_integer(self):
@@ -237,9 +237,8 @@ class TestUpdateOrderProductSerializer(unittest.TestCase):
 
         result = serializer.is_valid()
 
-        self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive integer."})
+        self.assertTrue(result)
+        self.assertEqual(serializer.errors, {})
 
     def test_update_shopping_price_max_decimal(self):
         self.data["shopping_price"] = 999999.99
@@ -247,9 +246,8 @@ class TestUpdateOrderProductSerializer(unittest.TestCase):
 
         result = serializer.is_valid()
 
-        self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive integer."})
+        self.assertTrue(result)
+        self.assertEqual(serializer.errors, {})
 
     def test_update_empty_shopping_price(self):
         self.data["shopping_price"] = None
@@ -258,18 +256,16 @@ class TestUpdateOrderProductSerializer(unittest.TestCase):
         result = serializer.is_valid()
 
         self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price username cannot be empty."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price cannot be empty."})
 
-    def test_update_sopping_price_string(self):
+    def test_update_shopping_price_string(self):
         self.data["shopping_price"] = "sfdfffs"
         serializer = UpdateOrderProductSerializer(data=self.data)
 
         result = serializer.is_valid()
 
         self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive decimal number."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive integer or decimal."})
 
     def test_update_shopping_price_not_positive_integer(self):
         self.data["shopping_price"] = 0
@@ -278,8 +274,7 @@ class TestUpdateOrderProductSerializer(unittest.TestCase):
         result = serializer.is_valid()
 
         self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive decimal number."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive integer or decimal."})
 
     def test_update_shopping_price_too_big_integer(self):
         self.data["shopping_price"] = 1000000
@@ -288,8 +283,7 @@ class TestUpdateOrderProductSerializer(unittest.TestCase):
         result = serializer.is_valid()
 
         self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive decimal number."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be less than 1000000.00."})
 
     def test_update_shopping_price_too_big_decimal(self):
         self.data["shopping_price"] = 1000000.00
@@ -298,14 +292,13 @@ class TestUpdateOrderProductSerializer(unittest.TestCase):
         result = serializer.is_valid()
 
         self.assertFalse(result)
-        self.assertNotEqual(serializer.validated_data, self.validated_data)
-        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be positive decimal number."})
+        self.assertEqual(serializer.errors, {"shopping_price": "Shopping price must be less than 1000000.00."})
 
 
 class TestDeleteOrderProductSerializer(unittest.TestCase):
     def test_delete_success(self):
         order_product_id = 1
-        serializer = DeleteOrderProductSerializer(order_product_id=order_product_id)
+        serializer = FindByIdAndDeleteOrderProductSerializer(order_product_id=order_product_id)
 
         result = serializer.is_valid()
 
@@ -314,7 +307,7 @@ class TestDeleteOrderProductSerializer(unittest.TestCase):
 
     def test_delete_empty_order_product_id(self):
         order_product_id = None
-        serializer = DeleteOrderProductSerializer(order_product_id=order_product_id)
+        serializer = FindByIdAndDeleteOrderProductSerializer(order_product_id=order_product_id)
 
         result = serializer.is_valid()
 
@@ -323,7 +316,7 @@ class TestDeleteOrderProductSerializer(unittest.TestCase):
 
     def test_delete_string(self):
         order_product_id = "sfdfffs"
-        serializer = DeleteOrderProductSerializer(order_product_id=order_product_id)
+        serializer = FindByIdAndDeleteOrderProductSerializer(order_product_id=order_product_id)
 
         result = serializer.is_valid()
 
@@ -332,7 +325,7 @@ class TestDeleteOrderProductSerializer(unittest.TestCase):
 
     def test_delete_not_positive_integer(self):
         order_product_id = 0
-        serializer = DeleteOrderProductSerializer(order_product_id=order_product_id)
+        serializer = FindByIdAndDeleteOrderProductSerializer(order_product_id=order_product_id)
 
         result = serializer.is_valid()
 
