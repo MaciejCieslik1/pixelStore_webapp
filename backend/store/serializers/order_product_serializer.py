c3797dd61e953840a3712491e19ce4dae96855f5
mcieslik <01187214@pw.edu.pl>
2025-09-17 17:58:10 +0200

    Deleted serializers to move the mto other one for id

--- backend/store/serializers/order_product_serializer.py
+++ backend/store/serializers/order_product_serializer.py
@@ -1,32 +1,6 @@
 from store.helper_classes.serializer_id_checker import SerializerHelper
 
 
-class FindByIdAndDeleteOrderProductSerializer:
-    def __init__(self, order_product_id: int):
-        self._id = order_product_id
-        self._error = None
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def error(self):
-        return self._error
-
-    @error.setter
-    def error(self, new_error):
-        self._error = new_error
-
-    def is_valid(self) -> bool:
-        error_messages = {"empty": "Order product id cannot be empty.",
-            "not_positive_int": "Order product id must be positive integer."}
-        self.error = SerializerHelper.return_id_error(self.id, error_messages)
-        if self.error:
-            return False
-        return True
-
-
 class CreateOrderProductSerializer:
     def __init__(self, data: dict):
         self._data = data
@@ -83,29 +57,11 @@ class CreateOrderProductSerializer:
         self.validated_data["shopping_price"] = price
         return True
 
-
-class UpdateOrderProductSerializer:
-    def __init__(self, data: dict):
-        self._data = data
-        self._validated_data = {}
-        self._errors = {}
-
-    @property
-    def data(self):
-        return self._data
-
-    @property
-    def validated_data(self):
-        return self._validated_data
-
-    @property
-    def errors(self):
-        return self._errors
-
-    def is_valid(self) -> bool:
+    def _validate_price(self):
         price = self.data.get("shopping_price")
         error = SerializerHelper.return_price_error(price)
         if error:
             self.errors["shopping_price"] = error
             return False
+        self.validated_data["shopping_price"] = self.data["shopping_price"]
         return True
