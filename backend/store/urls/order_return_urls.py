e539201a73dcf7b57444e065b520db101665e27a
mcieslik <01187214@pw.edu.pl>
2025-09-18 15:12:41 +0200

    Added new urls

---
+++ backend/store/urls/order_return_urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from store.service.order_return_service import FindByIdOrderReturnService, CreateOrderReturnService, \
+    UpdateOrderReturnService
+from store.views.order_return_view import FindByIdOrderReturnView, CreateOrderReturnView, UpdateOrderReturnView
+
+find_by_id_order_return_service = FindByIdOrderReturnService()
+create_service = CreateOrderReturnService()
+update_service = UpdateOrderReturnService()
+
+urlpatterns = [
+    path("find_by_id/<int:order_return_id>/", FindByIdOrderReturnView.as_view(
+        find_by_id_order_return_service=find_by_id_order_return_service), name="find_by_id_order_return/<int:order_return_id>"),
+    path("create/", CreateOrderReturnView.as_view(create_order_return_service=create_service),
+         name="create_order_return"),
+    path("update/<int:order_return_id>/", UpdateOrderReturnView.as_view(update_order_return_service=update_service),
+         name="update_order_return/<int:order_return_id>"),
+]
