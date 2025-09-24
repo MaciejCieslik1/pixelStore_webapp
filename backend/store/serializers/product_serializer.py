3ee1b23a99387b93e7b0d43618b8f8690343cc25
mcieslik <01187214@pw.edu.pl>
2025-09-24 12:24:10 +0200

    Created serializers template

---
+++ backend/store/serializers/product_serializer.py
@@ -0,0 +1,42 @@
+class FindAllProductsSerializer:
+    def __init__(self, data: dict):
+        self._data = data
+        self._validated_data = {}
+        self._errors = {}
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def validated_data(self):
+        return self._validated_data
+
+    @property
+    def errors(self):
+        return self._errors
+
+    def is_valid(self) -> bool:
+        return True
+
+
+class CreateProductSerializer:
+    def __init__(self, data: dict):
+        self._data = data
+        self._validated_data = {}
+        self._errors = {}
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def validated_data(self):
+        return self._validated_data
+
+    @property
+    def errors(self):
+        return self._errors
+
+    def is_valid(self) -> bool:
+        return True
