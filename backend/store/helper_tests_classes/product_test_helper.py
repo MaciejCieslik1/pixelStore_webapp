0ca43ff5abb1549703fa1aa39829921219f1df41
mcieslik <01187214@pw.edu.pl>
2025-10-03 19:04:26 +0200

    Fixed exemplary product issue

--- backend/store/helper_tests_classes/product_test_helper.py
+++ backend/store/helper_tests_classes/product_test_helper.py
@@ -5,6 +5,6 @@ class ProductTestHelper:
     @staticmethod
     def create_product(seller: User):
         product = Product(owner=seller, name="cpu", description="example", price=1000, amount=10, color="black",
-                            weight=0.45, length=15.00, width=15.00, height=0.80, guarantee_period=24, status="AVAILABLE")
+                            weight=0.45, length=15.0, width=15.0, height=0.8, guarantee_period=24, status="AVAILABLE")
         product.save()
         return product
