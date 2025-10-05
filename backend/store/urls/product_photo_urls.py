3cfee7061915c15fb611a5f0191e3e622636eeca
mcieslik <01187214@pw.edu.pl>
2025-10-05 19:48:20 +0200

    Added path argument to find all

--- backend/store/urls/product_photo_urls.py
+++ backend/store/urls/product_photo_urls.py
@@ -13,8 +13,8 @@ delete_service = DeleteProductPhotoService()
 urlpatterns = [
     path("find_by_id/<int:product_photo_id>/", FindByIdProductPhotoView.as_view(find_by_id_product_photo_service=find_by_id_product_photo_service),
          name="find_by_id_product_photo/<int:product_photo_id>"),
-    path("find_all_for_product/", FindAllForProductView.as_view(find_all_for_product_service=find_all_for_product_service),
-         name="find_all_for_product"),
+    path("find_all_for_product/<int:product_id>/", FindAllForProductView.as_view(find_all_for_product_service=find_all_for_product_service),
+         name="find_all_for_product/<int:product_id>"),
     path("create/", CreateProductPhotoView.as_view(create_product_photo_service=create_service), name="create_product_photo"),
     path("delete/<int:product_photo_id>/", DeleteProductPhotoView.as_view(delete_product_photo_service=delete_service),
          name="delete_product_photo/<int:product_photo_id>"),
