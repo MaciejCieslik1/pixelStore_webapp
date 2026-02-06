9babfbc4de439f91f4ab1e388819d2e02f8595bb
mcieslik <01187214@pw.edu.pl>
2026-02-06 21:33:23 +0100

    Added endpoints for product photo

--- backend/store/urls/product_photo_urls.py
+++ backend/store/urls/product_photo_urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from store.service.product_photo_service import FindByIdProductPhotoService, FindAllForProductService, \
-    CreateProductPhotoService, DeleteProductPhotoService
+    CreateProductPhotoService, DeleteProductPhotoService, FindMainPhotoProductPhotoService
 from store.views.product_photo_view import FindByIdProductPhotoView, FindAllForProductView, CreateProductPhotoView, \
-    DeleteProductPhotoView
+    DeleteProductPhotoView, FindMainPhotoProductPhotoView
 
 find_by_id_product_photo_service = FindByIdProductPhotoService()
 find_all_for_product_service = FindAllForProductService()
+find_main_photo_product_photo_service = FindMainPhotoProductPhotoService()
 create_service = CreateProductPhotoService()
 delete_service = DeleteProductPhotoService()
 
@@ -15,6 +16,8 @@ urlpatterns = [
          name="find_by_id_product_photo/<int:product_photo_id>"),
     path("find_all_for_product/<int:product_id>/", FindAllForProductView.as_view(find_all_for_product_service=find_all_for_product_service),
          name="find_all_for_product/<int:product_id>"),
+    path("find_main_photo/<int:product_id>/", FindMainPhotoProductPhotoView.as_view(find_main_photo_product_photo_service=find_main_photo_product_photo_service),
+         name="find_main_photo_product_photo/<int:product_id>"),
     path("create/", CreateProductPhotoView.as_view(create_product_photo_service=create_service), name="create_product_photo"),
     path("delete/<int:product_photo_id>/", DeleteProductPhotoView.as_view(delete_product_photo_service=delete_service),
          name="delete_product_photo/<int:product_photo_id>"),
