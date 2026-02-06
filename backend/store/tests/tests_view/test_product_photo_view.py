42e026d9c708414ab7d82339b984cc28d3664b17
mcieslik <01187214@pw.edu.pl>
2026-02-06 21:33:58 +0100

    Added tests for controller

--- backend/store/tests/tests_view/test_product_photo_view.py
+++ backend/store/tests/tests_view/test_product_photo_view.py
@@ -8,13 +8,15 @@ from store.exceptions import IncorrectTokenError, TokenExpiredError, CannotGetTo
     TokenExpiredByReplacementError, InvalidInputData
 from store.helper_tests_classes.authentication_test_helper import create_api_client_with_user
 from store.helper_tests_classes.product_photo_test_helper import ProductPhotoTestHelper
+from store.helper_tests_classes.product_test_helper import ProductTestHelper
 
 
 @pytest.mark.django_db
 class TestFindByIdProductPhoto:
     def setup_method(self):
         self.client, self.user = create_api_client_with_user()
-        self.product_photo = ProductPhotoTestHelper.create_product_photo(self.user)
+        product = ProductTestHelper.create_product(self.user)
+        self.product_photo = ProductPhotoTestHelper.create_product_photo(product, "test_url")
         self.product_photo_id = self.product_photo.product_photo_id
         self.product_photo_data = {"product_photo_id": self.product_photo_id,
             "product_id": self.product_photo.product.product_id, "image_url": self.product_photo.image_url,
@@ -95,7 +97,8 @@ class TestFindByIdProductPhoto:
 class TestFindAllProductPhotos:
     def setup_method(self):
         self.client, self.user = create_api_client_with_user()
-        self.product_photo = ProductPhotoTestHelper.create_product_photo(self.user)
+        product = ProductTestHelper.create_product(self.user)
+        self.product_photo = ProductPhotoTestHelper.create_product_photo(product, "test_url")
         self.product_photo_id = self.product_photo.product_photo_id
         self.product_id = self.product_photo.product.product_id
         self.product_photos_data = [{"product_photo_id": self.product_photo_id,
@@ -168,10 +171,93 @@ class TestFindAllProductPhotos:
 
 
 @pytest.mark.django_db
+class TestFindMainPhotoProductPhoto:
+    def setup_method(self):
+        self.client, self.user = create_api_client_with_user()
+        product = ProductTestHelper.create_product(self.user)
+        self.product_photo = ProductPhotoTestHelper.create_product_photo(product, "test_url")
+        self.product_id = self.product_photo.product_id
+        self.product_photo_data = {"product_photo_id": self.product_photo.product_photo_id,
+            "product_id": self.product_photo.product.product_id, "image_url": self.product_photo.image_url,
+            "is_main_photo": self.product_photo.is_main_photo}
+
+    @patch("store.service.product_photo_service.FindMainPhotoProductPhotoService.find_main_photo")
+    def test_find_main_photo_success(self, mock_find_by_id):
+        mock_find_by_id.return_value = self.product_photo_data
+
+        response = self.client.get(f"/product_photo/find_main_photo/{self.product_id}/")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == self.product_photo_data
+        mock_find_by_id.assert_called_once()
+
+    @patch("store.service.product_photo_service.FindMainPhotoProductPhotoService.find_main_photo")
+    def test_find_main_photo_invalid_token(self, mock_find_by_id):
+        mock_find_by_id.side_effect = IncorrectTokenError("Access token error.")
+
+        response = self.client.get(f"/product_photo/find_main_photo/{self.product_id}/")
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.data["error"] == "Access token error."
+
+    @patch("store.service.product_photo_service.FindMainPhotoProductPhotoService.find_main_photo")
+    def test_find_main_photo_expired_token(self, mock_find_by_id):
+        mock_find_by_id.side_effect = TokenExpiredError("Access token error.")
+
+        response = self.client.get(f"/product_photo/find_main_photo/{self.product_id}/")
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.data["error"] == "Access token error."
+
+    @patch("store.service.product_photo_service.FindMainPhotoProductPhotoService.find_main_photo")
+    def test_find_main_photo_cannot_get_token_from_request(self, mock_find_by_id):
+        mock_find_by_id.side_effect = CannotGetTokenFromRequestError("Access token error.")
+
+        response = self.client.get(f"/product_photo/find_main_photo/{self.product_id}/")
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.data["error"] == "Access token error."
+
+    @patch("store.service.product_photo_service.FindMainPhotoProductPhotoService.find_main_photo")
+    def test_find_main_photo_token_expired_by_replacement(self, mock_find_by_id):
+        mock_find_by_id.side_effect = TokenExpiredByReplacementError("Access token error.")
+
+        response = self.client.get(f"/product_photo/find_main_photo/{self.product_id}/")
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.data["error"] == "Access token error."
+
+    @patch("store.service.product_photo_service.FindMainPhotoProductPhotoService.find_main_photo")
+    def test_find_main_photo_other_exception(self, mock_find_by_id):
+        mock_find_by_id.side_effect = DatabaseError("DB connection failed")
+
+        response = self.client.get(f"/product_photo/find_main_photo/{self.product_id}/")
+
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert response.data["error"] == "Unexpected error."
+
+    @patch("store.service.product_photo_service.FindMainPhotoProductPhotoService.find_main_photo")
+    def test_find_main_photo_invalid_input_data(self, mock_find_by_id):
+        mock_find_by_id.side_effect = InvalidInputData("Invalid input data provided.")
+
+        response = self.client.get(f"/product_photo/find_main_photo/{self.product_id}/")
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data["error"] == "Invalid input data provided."
+
+    def test_find_main_photo_invalid_serializer(self):
+        self.product_id = 0
+        response = self.client.get(f"/product_photo/find_main_photo/{self.product_id}/")
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
 class TestCreateProductPhoto:
     def setup_method(self):
         self.client, self.user = create_api_client_with_user()
-        self.product_photo = ProductPhotoTestHelper.create_product_photo(self.user)
+        product = ProductTestHelper.create_product(self.user)
+        self.product_photo = ProductPhotoTestHelper.create_product_photo(product, "test_url")
         self.product_photo_id = self.product_photo.product_photo_id
         self.product_photo_creation_data = {"product_photo_id": self.product_photo_id,
             "product_id": self.product_photo.product.product_id,
@@ -255,7 +341,8 @@ class TestCreateProductPhoto:
 class TestDeleteProductPhoto:
     def setup_method(self):
         self.client, self.user = create_api_client_with_user()
-        self.product_photo = ProductPhotoTestHelper.create_product_photo(self.user)
+        product = ProductTestHelper.create_product(self.user)
+        self.product_photo = ProductPhotoTestHelper.create_product_photo(product, "test_url")
         self.product_photo_id = self.product_photo.product_photo_id
         self.product_communicate = "Product photo deleted successfully"
 
