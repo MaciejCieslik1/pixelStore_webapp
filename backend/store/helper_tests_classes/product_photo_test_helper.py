31ae1e0cf67db108115c7f27f100477a082041e1
mcieslik <01187214@pw.edu.pl>
2026-02-06 21:34:21 +0100

    Modified poduct photo test helper

--- backend/store/helper_tests_classes/product_photo_test_helper.py
+++ backend/store/helper_tests_classes/product_photo_test_helper.py
@@ -1,11 +1,9 @@
-from store.helper_tests_classes.product_test_helper import ProductTestHelper
-from store.models import ProductPhoto, User
+from store.models import ProductPhoto, Product
 
 
 class ProductPhotoTestHelper:
     @staticmethod
-    def create_product_photo(seller: User) -> ProductPhoto:
-        product = ProductTestHelper.create_product(seller)
-        product_photo = ProductPhoto(product=product, image_url="example_url", is_main_photo=True)
+    def create_product_photo(product: Product, image_url: str) -> ProductPhoto:
+        product_photo = ProductPhoto(product=product, image_url=image_url, is_main_photo=True)
         product_photo.save()
         return product_photo
