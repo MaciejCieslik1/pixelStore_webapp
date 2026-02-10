db12e9529caecd6adda96d3b5b8ac0dda2d000e5
mcieslik <01187214@pw.edu.pl>
2026-02-10 16:20:26 +0100

    Fixed urls

--- backend/store/urls/product_review_urls.py
+++ backend/store/urls/product_review_urls.py
@@ -15,12 +15,12 @@ urlpatterns = [
     path("find_by_id/<int:product_review_id>/", FindByIdProductReviewView.as_view(
         find_by_id_product_review_service=find_by_id_product_review_service),
         name="find_by_id_product_review/<int:product_review_id>"),
-    path("find_all_for_product/", FindAllProductReviewsView.as_view(
+    path("find_all_for_product/<int:product_id>/", FindAllProductReviewsView.as_view(
         find_all_product_reviews_service=find_all_product_reviews_service),
-         name="find_all_product_reviews"),
-    path("find_all_from_user/", FindAllFromUserProductReviewsView.as_view(
+         name="find_all_product_reviews/<int:product_id>"),
+    path("find_all_from_user/<str:reviewer_username>/", FindAllFromUserProductReviewsView.as_view(
         find_all_from_user_product_reviews_service=find_all_from_user_product_reviews_service),
-         name="find_all_from_user_product_reviews"),
+         name="find_all_from_user_product_reviews/<str:reviewer_username>"),
     path("create/", CreateProductReviewView.as_view(create_product_review_service=create_service),
          name="create_review_product"),
     path("delete/<int:product_review_id>/", DeleteProductReviewView.as_view(delete_product_review_service=delete_service),
