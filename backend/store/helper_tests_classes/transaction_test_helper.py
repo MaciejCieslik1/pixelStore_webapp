ff192d665d0fbae36ec79bab09da0f5504bbf4e5
mcieslik <01187214@pw.edu.pl>
2026-02-11 20:47:53 +0100

    Added helper class for transaction creation

---
+++ backend/store/helper_tests_classes/transaction_test_helper.py
@@ -0,0 +1,13 @@
+from django.utils import timezone
+from decimal import Decimal
+
+from store.models import User, Transaction
+
+
+class TransactionTestHelper:
+    @staticmethod
+    def create_transaction(buyer: User) -> Transaction:
+        transaction = Transaction(buyer=buyer, total_price=Decimal('1000'), date_time=timezone.now(),
+                                  is_finished=False)
+        transaction.save()
+        return transaction
