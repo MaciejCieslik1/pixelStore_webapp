1c1a12134f4499ee0395e023b27ece0307c58cdf
mcieslik <01187214@pw.edu.pl>
2026-02-16 15:36:47 +0100

    Changed transaction test helepr

--- backend/store/helper_tests_classes/transaction_test_helper.py
+++ backend/store/helper_tests_classes/transaction_test_helper.py
@@ -7,7 +7,7 @@ from store.models import User, Transaction
 class TransactionTestHelper:
     @staticmethod
     def create_transaction(buyer: User) -> Transaction:
-        transaction = Transaction(buyer=buyer, total_price=Decimal('1000'), date_time=timezone.now(),
+        transaction = Transaction(buyer=buyer, total_price=Decimal('1'), date_time=timezone.now(),
                                   is_finished=False)
         transaction.save()
         return transaction
