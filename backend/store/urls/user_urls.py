2cd55a095b3302e30567c7cb5c143a35a9d25086
mcieslik <01187214@pw.edu.pl>
2026-02-17 20:59:43 +0100

    Fixed delete_service bug

--- backend/store/urls/user_urls.py
+++ backend/store/urls/user_urls.py
@@ -5,12 +5,12 @@ from store.views.user_view import FindByUsernameUserView, UpdateUserView, Delete
 
 find_by_username_user_service = FindByUsernameUserService()
 update_service = UpdateUserService()
-delete_service = DeleteAccountUserService()
+delete_account_user_service = DeleteAccountUserService()
 
 urlpatterns = [
     path("find_by_username/<str:username>/", FindByUsernameUserView.as_view(
         find_by_username_user_service=find_by_username_user_service), name="find_by_username/<str:username>"),
     path("update/", UpdateUserView.as_view(update_user_service=update_service), name="update_user"),
-    path("delete_account/", DeleteAccountUserView.as_view(delete_user_service=delete_service),
+    path("delete_account/", DeleteAccountUserView.as_view(delete_account_user_service=delete_account_user_service),
          name="delete_account")
 ]
