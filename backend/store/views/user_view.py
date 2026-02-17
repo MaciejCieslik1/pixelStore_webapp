f76b915ddaab41c5a7ec9a1b3de1829c88a723a7
mcieslik <01187214@pw.edu.pl>
2026-02-17 21:00:52 +0100

    Implemented user view

--- backend/store/views/user_view.py
+++ backend/store/views/user_view.py
@@ -7,11 +7,8 @@ from rest_framework.views import APIView
 from store.exceptions import IncorrectTokenError, TokenExpiredError, CannotGetTokenFromRequestError, \
     TokenExpiredByReplacementError, InvalidInputData
 from store.helper_classes.authentication_helper import TokenUtils
-from store.serializers.check_id_serializer import CheckIdSerializer
-from store.serializers.page_serializer import PageSerializer
-from store.serializers.transaction_serializer import CreateTransactionSerializer, UpdateTransactionSerializer
-from store.service.transaction_service import FindByIdTransactionService, FindAllMineTransactionsService, \
-    CreateTransactionService, UpdateTransactionService
+from store.serializers.check_username_serializer import CheckUsernameSerializer
+from store.serializers.user_serializer import UpdateUserSerializer
 from store.service.user_service import FindByUsernameUserService, UpdateUserService, DeleteAccountUserService
 
 
@@ -27,7 +24,30 @@ class FindByUsernameUserView(APIView):
         return self._find_by_username_user_service
 
     def get(self, request: Request, username: str) -> Response:
-        pass
+        serializer = CheckUsernameSerializer(username=username)
+        if serializer.is_valid():
+            try:
+                token = TokenUtils.get_jwt_token_from_request(request)
+                user_found = self.find_by_username_user_service.find_by_username(token, request.user, username)
+                return Response(user_found, status=status.HTTP_200_OK)
+            except (IncorrectTokenError, TokenExpiredError, CannotGetTokenFromRequestError,
+                    TokenExpiredByReplacementError) as e:
+                return Response(
+                    {"error": "Access token error.", "details": str(e)},
+                    status=status.HTTP_401_UNAUTHORIZED
+                )
+            except InvalidInputData as e:
+                return Response(
+                    {"error": "Invalid input data provided.", "details": str(e)},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            except Exception as e:
+                return Response(
+                    {"error": "Unexpected error.", "details": str(e)},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+        else:
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UpdateUserView(APIView):
@@ -42,7 +62,30 @@ class UpdateUserView(APIView):
         return self._update_user_service
 
     def put(self, request: Request) -> Response:
-        pass
+        serializer = UpdateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                token = TokenUtils.get_jwt_token_from_request(request)
+                communicate = self.update_user_service.update(token, request.user, serializer.validated_data)
+                return Response({"msg": communicate}, status=status.HTTP_200_OK)
+            except (IncorrectTokenError, TokenExpiredError, CannotGetTokenFromRequestError,
+                    TokenExpiredByReplacementError) as e:
+                return Response(
+                    {"error": "Access token error.", "details": str(e)},
+                    status=status.HTTP_401_UNAUTHORIZED
+                )
+            except InvalidInputData as e:
+                return Response(
+                    {"error": "Invalid input data provided.", "details": str(e)},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            except Exception as e:
+                return Response(
+                    {"error": "Unexpected error.", "details": str(e)},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DeleteAccountUserView(APIView):
@@ -50,11 +93,30 @@ class DeleteAccountUserView(APIView):
 
     def __init__(self, delete_account_user_service: DeleteAccountUserService, **kwargs):
         super().__init__(**kwargs)
-        self._delete_service = delete_account_user_service
+        self._delete_account_user_service = delete_account_user_service
 
     @property
     def delete_account_user_service(self):
-        return self._delete_service
+        return self._delete_account_user_service
 
     def delete(self, request: Request) -> Response:
-        pass
+        try:
+            token = TokenUtils.get_jwt_token_from_request(request)
+            communicate = self.delete_account_user_service.delete(token, request.user)
+            return Response({"msg": communicate}, status=status.HTTP_200_OK)
+        except (IncorrectTokenError, TokenExpiredError, CannotGetTokenFromRequestError,
+                TokenExpiredByReplacementError) as e:
+            return Response(
+                {"error": "Access token error.", "details": str(e)},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+        except InvalidInputData as e:
+            return Response(
+                {"error": "Invalid input data provided.", "details": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except Exception as e:
+            return Response(
+                {"error": "Unexpected error.", "details": str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
