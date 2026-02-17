3a3c6b1a0a42838b333f4b41703d242f2fb9a68a
mcieslik <01187214@pw.edu.pl>
2026-02-17 21:57:06 +0100

    Implemented user service

--- backend/store/service/user_service.py
+++ backend/store/service/user_service.py
@@ -1,24 +1,37 @@
-import datetime
-
-from django.core.paginator import Paginator
-from django.db.models import Q
-
 from store.exceptions import InvalidInputData
 from store.helper_classes.authentication_helper import TokenUtils
-from store.models import User, Transaction, OrderProduct
-from store.output_serializers.transaction_output_serializer import TransactionOutputSerializer
+from store.models import User
+from store.output_serializers.user_output_serializer import UserOutputSerializer
 
 
 class FindByUsernameUserService:
     def find_by_username(self, token: str, user: User, username: str) -> dict:
-        pass
+        TokenUtils.verify_access_token(token, user)
+
+        user = User.objects.filter(username=username).first()
+        if user is None:
+            raise InvalidInputData("User with this username does not exist.")
+
+        serializer = UserOutputSerializer(user)
+        return serializer.data
 
 
 class UpdateUserService:
     def update(self, token : str, user: User, update_data: dict) -> str:
-        pass
+        TokenUtils.verify_access_token(token, user)
+
+        user.money = update_data["money"]
+        user.bio = update_data["bio"]
+
+        user.save()
+
+        return "User data updated successfully."
 
 
 class DeleteAccountUserService:
     def delete(self, token : str, user: User) -> str:
-        pass
+        TokenUtils.verify_access_token(token, user)
+
+        user.delete()
+
+        return "Deleted account successfully."
