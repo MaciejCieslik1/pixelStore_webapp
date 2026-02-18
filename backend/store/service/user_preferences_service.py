88e790e44f3d9cc5166a1c6c222a1fc8c4cf756b
mcieslik <01187214@pw.edu.pl>
2026-02-18 11:26:27 +0100

    Implemented user serializer

--- backend/store/service/user_preferences_service.py
+++ backend/store/service/user_preferences_service.py
@@ -1,13 +1,25 @@
-from store.exceptions import InvalidInputData
 from store.helper_classes.authentication_helper import TokenUtils
-from store.models import User
+from store.models import User, UserPreferences
+from store.output_serializers.user_preferences_output_serializer import UserPreferencesOutputSerializer
 
 
 class FindUserPreferencesService:
     def find(self, token: str, user: User) -> dict:
-        pass
+        TokenUtils.verify_access_token(token, user)
+
+        user_preferences = UserPreferences.objects.filter(user_id=user.user_id).first()
+
+        serializer = UserPreferencesOutputSerializer(user_preferences)
+        return serializer.data
 
 
 class UpdateUserPreferencesService:
-    def update(self, token: str, user: User, new_user_preferences: dict) -> dict:
-        pass
+    def update(self, token: str, user: User, new_user_preferences: dict) -> str:
+        TokenUtils.verify_access_token(token, user)
+
+        user_preferences = UserPreferences.objects.filter(user_id=user.user_id).first()
+
+        user_preferences.dark_mode = new_user_preferences["dark_mode"]
+        user_preferences.save()
+
+        return "User preferences updated successfully."
