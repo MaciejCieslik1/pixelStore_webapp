79177418440e02775d1986246203647ba299a8ff
mcieslik <01187214@pw.edu.pl>
2026-03-03 19:03:23 +0100

    Added VerifyAccountPage to routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {VerifyAccountPage} from "./pages/Authentication/VerifyAccountPage";
+import {VerifyAccountPage} from "./pages/Authentication/VerifyAccountPage.tsx";
 import {RegisterPage} from "./pages/Authentication/RegisterPage.tsx";
 import {LoginPage} from "./pages/Authentication/LoginPage.tsx";
 
