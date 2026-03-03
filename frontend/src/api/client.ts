5f9a221e5678787a6b639a689436b3778423ae97
mcieslik <01187214@pw.edu.pl>
2026-03-03 14:24:36 +0100

    Added client

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,6 +1,19 @@
-export const API_URL = "http://localhost:8000";
+export const API_URL =  "http://localhost:8000";
 
-export async function apiGet(path: string) {
-  const res = await fetch(`${API_URL}${path}`, { credentials: "include" });
+interface RequestOptions extends RequestInit {
+    body?: any;
+}
+
+export async function apiFetch<T>(url: string, options: RequestOptions = {}): Promise<T> {
+  const res = await fetch(`${API_URL}${url}`, {
+    headers: {"Content-Type": "application/json", ...(options.headers || {})},
+    credentials: "include", ...options,
+    body: options.body ? JSON.stringify(options.body) : undefined, });
+
+  if (!res.ok) {
+    const err = await res.json().catch(() => ({}));
+    throw { status: res.status, ...err };
+  }
+
   return res.json();
 }
