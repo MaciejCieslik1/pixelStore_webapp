7ddfaf7f9c6c4c08f64ccbd5ff0299e18674cc4c
mcieslik <01187214@pw.edu.pl>
2026-03-04 15:46:49 +0100

    Added new paths to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import {VerifyAccountPage} from "./pages/Authentication/VerifyAccountPage.tsx";
 import {RegisterPage} from "./pages/Authentication/RegisterPage.tsx";
 import {LoginPage} from "./pages/Authentication/LoginPage.tsx";
+import {ProductListPage} from "./pages/Product/ProductListPage.tsx";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/verify_account" element={<VerifyAccountPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/product/find_all" element={<ProductListPage />} />
       </Routes>
     </Router>
   );
