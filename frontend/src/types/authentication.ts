5a15f4e787421862f4609070e30bbd30159aa11a
mcieslik <01187214@pw.edu.pl>
2026-03-04 20:05:06 +0100

    Added spaces

--- frontend/src/types/authentication.ts
+++ frontend/src/types/authentication.ts
@@ -1,34 +1,34 @@
 export interface RegisterData {
-  username: string;
-  email: string;
-  password: string;
-  address: string;
-  postal_code: string;
-  city: string;
-  country: string;
+    username: string;
+    email: string;
+    password: string;
+    address: string;
+    postal_code: string;
+    city: string;
+    country: string;
 }
 
 export interface VerifyAccountData {
-  email: string;
-  verification_code: string;
+    email: string;
+    verification_code: string;
 }
 
 export interface LoginData {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 export interface Tokens {
-  access_token: string;
-  refresh_token: string;
+    access_token: string;
+    refresh_token: string;
 }
 
 export interface VerifyTokenData {
-  token: string;
+      token: string;
 }
 
 export interface ResetPasswordData {
-  new_password: string;
-  confirm_password: string;
-  verification_code: string;
+    new_password: string;
+    confirm_password: string;
+    verification_code: string;
 }
