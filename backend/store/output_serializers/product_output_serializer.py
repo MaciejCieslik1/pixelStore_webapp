a8e679ba336d941f382e81aaca5f229bf477f6f1
mcieslik <01187214@pw.edu.pl>
2026-03-04 20:02:02 +0100

    Added product photos

--- backend/store/output_serializers/product_output_serializer.py
+++ backend/store/output_serializers/product_output_serializer.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 
 from store.models import Product
+from store.output_serializers.product_photo_output_serializer import ProductPhotoOutputSerializer
 
 
 class ProductOutputSerializer(serializers.ModelSerializer):
     owner_username = serializers.CharField(source="owner.username")
+    product_photos = ProductPhotoOutputSerializer(many=True, read_only=True)
 
     class Meta:
         model = Product
         fields = ["product_id", "owner_username", "name", "description", "price", "amount", "color", "weight", "length", "width",
-            "height", "guarantee_period", "status"]
+            "height", "guarantee_period", "status", "product_photos"]
