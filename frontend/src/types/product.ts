ccff3cd371954414e3082adf73cfdeb7e01421bf
mcieslik <01187214@pw.edu.pl>
2026-03-05 14:47:22 +0100

    Added ProductDetailsData interface

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -12,3 +12,19 @@ export interface ProductImage {
     image_url: string;
     is_main_photo: boolean;
 }
+
+export interface ProductDetailsData {
+    product_id: number;
+    sellerUsername: string;
+    name: string;
+    description: string;
+    price: number;
+    amount: number;
+    color: string;
+    weight: number;
+    length: number;
+    height: number;
+    guarantee_period: number;
+    status: string;
+    product_photos?: ProductImage[];
+}
