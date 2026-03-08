6cffe8338b362e3710b3ca596fc390d9965e3742
mcieslik <01187214@pw.edu.pl>
2026-03-08 20:22:24 +0100

    Added REST API methods to cors configurations

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,6 +64,15 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
